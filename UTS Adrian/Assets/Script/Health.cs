3177e9a2d6a0658d3b632d45ef368c4ff91d94b9
Adrianbp <85493901+Adriansyah160102@users.noreply.github.com>
2021-10-17 16:06:29 +0700

    UTS_PemGame
    Adriansyah Bagus Purwanto_19050974048_PTI19

---
+++ UTS Adrian/Assets/Script/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    [SerializeField] Slider HealthBar;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HealthBar.value = health;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health -= 1;
+    }
+}
